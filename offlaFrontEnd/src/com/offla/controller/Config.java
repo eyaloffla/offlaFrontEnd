665199cf2d669a66c2f7dddd346404edfaefe07e
eyal <eyal.r.offla@gmail.com>
2015-12-09 17:14:28 +0200

    wed dec 9 17:14

--- offlaFrontEnd/src/com/offla/controller/Config.java
+++ offlaFrontEnd/src/com/offla/controller/Config.java
@@ -2,11 +2,13 @@ package com.offla.controller;
 
 public class Config {
 	
-	public static final String DOMAIN = "http://localhost:9080";
+	public static final String DOMAIN = "http://websphere-application-server-03.mybluemix.net";
+	
+	//public static final String DOMAIN = "http://localhost:9080";
 	
 	public static final String DOMAIN_FRONT_END = DOMAIN + "/offlaFrontEnd";
 	
-	public static final String DOMAIN_ROOT_REMOTE_WS = "http://localhost:9080/offlaserver01WS";
+	public static final String DOMAIN_ROOT_REMOTE_WS = DOMAIN + "/offlaserver01WS";
 	
 	public static final String URL_IS_PERSON_ON_DB_WS = DOMAIN_ROOT_REMOTE_WS + "/rest/personexist/ispersonondb";
 
